511a8bc92ee056ec26c4c960d4be8c44ed7091ae
type9 <gabespersonal@gmail.com>
2020-02-17 14:16:15 -0800

    added authentication

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //Require
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 //db
 require('./data/reddit-db');
 //Setup
@@ -24,8 +27,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //Validator
 app.use(expressValidator());
+//JWT
+app.use(cookieParser()); // Add this after you initialize express.
 
 //Controllers
+require('./controllers/auth.js')(app);
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
 
